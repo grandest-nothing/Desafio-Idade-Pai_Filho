8faba93067064a13608da5f1530877d8d0d17488
Grandest Nothing <rodolforodex63@gmail.com>
2022-11-21 18:13:26 -0300

    modified:   script.js

--- script.js
+++ script.js
@@ -10,25 +10,21 @@ const anoAtual = dataAtual.getFullYear();
 const calc = (parentAgeVar, childAgeVar) => {
   parentAgeVar = Number(parentAge.value);
   childAgeVar = Number(childAge.value);
-  // const childAge2 = childAgeVar * 2;
-  const difAge = parentAgeVar - childAgeVar * 2;
-  // if (difAge < 0) {
-  const pai = parentAgeVar + difAge;
-  const filho = childAgeVar + difAge;
-  const ano = anoAtual + difAge;
-  you.innerHTML = `você: ${filho}`;
-  parent.innerHTML = `seu pai: ${pai}`;
-  year.innerHTML = `ano que ocorreu: ${ano}`;
-  // } else {
-  //   const pai = parentAgeVar - difAge;
-  //   const filho = childAgeVar - difAge;
-  //   const ano = anoAtual - difAge;
-  //   you.innerHTML = `você: ${filho}`;
-  //   parent.innerHTML = `seu pai: ${pai}`;
-  //   year.innerHTML = `ano que ocorreu: ${ano}`;
-  //   console.log(typeof filho);
-  //   console.log(filho);
-  // }
+  if (Number(parentAgeVar) && Number(childAgeVar)) {
+    const difAge = parentAgeVar - childAgeVar * 2;
+    const pai = parentAgeVar + difAge;
+    const filho = childAgeVar + difAge;
+    const ano = anoAtual + difAge;
+    you.innerHTML = `você: ${filho}`;
+    parent.innerHTML = `seu pai: ${pai}`;
+    year.innerHTML = `ano que ocorreu: ${ano}`;
+    parentAge.value = "";
+    childAge.value = "";
+  } else {
+    alert("Você deveria digitar um número!");
+    parentAge.value = "";
+    childAge.value = "";
+  }
 };
 calcular.addEventListener("click", (e) => {
   e.preventDefault();
